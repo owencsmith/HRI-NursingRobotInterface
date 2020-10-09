a1f552e87a53430aeb4c0a40434b0f1c5302b835
Gabriel Entov <gentov@wpi.edu>
2020-10-09 11:57:12 -0400

    continued pseudocode

--- middleman/src/Middleman.py
+++ middleman/src/Middleman.py
@@ -13,17 +13,20 @@ class Middleman():
         self.robotsForOperator = []
 
         #TODO:
-        #subscribers (should actually be Service/Client)
         rospy.Subscriber("/supervisor/nav_task", String, self.processNavTask)
         rospy.Subscriber("/supervisor/sos", String, self.passRobotToQueueForOperator)
-        rospy.Subscriber("/operator/done_helping", String, self.changeRobotToIDLE)
+        rospy.Subscriber("/operator/done_helping", String, self.advanceRobotHelpQueue)
+        rospy.Subscriber("/operator/request_extra_views_from_robot", String, self.sendAnotherRobotForCameraViews)
+        rospy.Subscriber("/robot/stuck", String, self.passRobotToQueueForOperator)
+        rospy.Subscriber("/robot/done_task", String, self.alertSupervisorRobotIsDone)
 
         #publishers
         # TODO: Make our own Robot message with fields of robot object
         # TODO: when operator is done with a robot, pop from list and send new one using Service/Client (not publisher)
-        sendNewRobotToOperator = rospy.Publisher('/operator/new_robot', String, queue_size=10)
-
-
+        self.sendNewRobotToOperator  = rospy.Publisher('/operator/new_robot', String, queue_size=10)
+        self.robotIsAvailableForExtraViews = rospy.Publisher('/operator/robot_is_available_for_extra_views', Bool, queue_size = 10)
+        self.robotsLeftInQueue = rospy.Publisher('/operator/robots_left_in_queue', Int32, queue_size = 10)
+        self.robotsLeftInQueue = rospy.Publisher('/supervisor/robots_finished_task', String, queue_size=10)
         pass
 
 
@@ -52,20 +55,30 @@ class Middleman():
 
         pass
 
-    def changeRobotToIDLE(self,data):
+    def advanceRobotHelpQueue(self,data):
         #parse robot name
         robotName = ""
         robotThatWasHelped = self.activeRobotDictionary[robotName]
         #This gets published, no need to update supervisor
         robotThatWasHelped.status = "OK"
         robotThatWasHelped.currentTask = "IDL"
+        # pop another robot from the queue if queue is not empty
+        # publish that robot to /operator/new_robot
         pass
 
-    # call in whatever loop this node uses
-    # TODO: Nick, depending on the name of the robot, add a list of robots on the left side of the GUI
-    def publishRobotStates(self):
-        # ask each robot to publish in a list comprehension
-        [robot.publish() for robot in self.activeRobotDictionary.values()]
+    def sendAnotherRobotForCameraViews(self):
+        # Is there an IDLE robot that CAN be used?
+            # find it and grab it from the dictionary
+            # publish that a robot is on its way
+        # If not
+            # publish that no robot could be used right now
+
+        # parse the string for the robot to navigate to for more views
+        # grab the pose of the robot to navigate to
+        # Tell the IDLE robot to navigate to the robot that needs more views
+        # set the task of the IDLE robot to NAV
+        # set the status of the IDLE robot to OPC
+        pass
 
     def guiCoordinatesToWorldCoordinates(self, coordinates):
         pass
@@ -80,4 +93,20 @@ class Middleman():
         # if else chain depending on task or dictionary of task code
         # linking to function
         pass
+
+    def alertSupervisorRobotIsDone(self):
+        #find robot in dicionary
+        #change robot task to IDLE
+        # publish to /supervisor/robots_finished_task that the robot is done
+        pass
+    # call the methods below in whatever loop this node uses
+    # TODO: Nick, depending on the name of the robot, add a list of robots on the left side of the GUI
+    def publishRobotStates(self):
+        # ask each robot to publish in a list comprehension
+        [robot.publish() for robot in self.activeRobotDictionary.values()]
+
+    def publishRobotsLeftInQueue(self):
+        self.robotsLeftInQueue.publish()
+
+
 
--- middleman/src/Robot.py
+++ middleman/src/Robot.py
@@ -9,7 +9,8 @@ class Robot():
         dictionaryOfPublishers = []
 
         #publishers
-        statePublisher = rospy.Publisher('/operator/robotState', String, queue_size=10)
+        statePublisherForOperator = rospy.Publisher('/operator/robotState', String, queue_size=10)
+        statePublisherForSupervisor = rospy.Publisher('/supervisor/robotState', String, queue_size=10)
 
 
     # idk how to implement this cleanly but we will
