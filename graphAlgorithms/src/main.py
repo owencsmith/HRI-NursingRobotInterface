1e85d69c43f0234525bd36af344f949fec007ca5
Taylor <tberg1234@gmail.com>
2021-04-14 21:34:21 -0400

    working vertices for trap decomp

--- graphAlgorithms/src/main.py
+++ graphAlgorithms/src/main.py
@@ -3,7 +3,7 @@
 from PIL import Image
 import numpy as np
 from trapezoidalDecomposition import TrapezoidalDecomposition
-
+from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 
 
@@ -21,20 +21,62 @@ def load_map(file_path, resolution_scale):
     map_array = np.asarray(img, dtype='uint8')
 
     # Get bianry image
-    threshold = 230
+    threshold = 240 #230
     map_array = 1 * (map_array > threshold)
 
     # Result 2D numpy array
     return map_array
+
+def draw_path(grid, title, lines = list()):
+    # Visualization of the found path using matplotlib
+    fig, ax = plt.subplots(1)
+    ax.margins()
+    # Draw map
+    row = len(grid)     # map size
+    col = len(grid[0])  # map size
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j]:
+                ax.add_patch(Rectangle((j-0.5, i-0.5),1,1,edgecolor='w',facecolor='w'))  # free space
+            else:
+                ax.add_patch(Rectangle((j-0.5, i-0.5),1,1,edgecolor='w',facecolor='k'))  # obstacle
+    # Draw path
+    # for x, y in path:
+    #     ax.add_patch(Rectangle((y-0.5, x-0.5),1,1,edgecolor='k',facecolor='b'))          # path
+    # ax.add_patch(Rectangle((start[1]-0.5, start[0]-0.5),1,1,edgecolor='k',facecolor='g'))# start
+    # ax.add_patch(Rectangle((goal[1]-0.5, goal[0]-0.5),1,1,edgecolor='k',facecolor='r'))  # goal
+    # Graph settings
+
+    # for line in lines:
+    #     for pt in line:
+    #         x = pt[0]
+    #         y = pt[1]
+    #         ax.add_patch(Rectangle((y - 0.5, x - 0.5), 1, 1, edgecolor='k', facecolor='b'))
+
+    for pt in lines:
+        x = pt[0]
+        y = pt[1]
+        ax.add_patch(Rectangle((y - 0.5, x - 0.5), 1, 1, edgecolor='k', facecolor='b'))
+
+
+    plt.title(title)
+    plt.axis('scaled')
+    plt.gca().invert_yaxis()
+    plt.show()
 
 
 if __name__ == "__main__":
     # Load the map
     start = (200, 75)
     goal  = (30, 250)
-    map_array = load_map("HospitalMap.png", 0.1)
+    map_array = load_map("HospitalMapCleaned_filledin_cropped.png", 0.1)
+    # map_array = load_map("test_map_simple.png", 0.1)
+    draw_path(map_array,"Hospital Array")
 
     trap_decomp_graph = TrapezoidalDecomposition(map_array)
+    line_lists_for_boundaries = trap_decomp_graph.create_trapezoids() # trap_decomp_graph = TrapezoidalDecomposition(map_array)
+    draw_path(map_array,"Hospital Array",line_lists_for_boundaries)
+
 
     # # Planning class
     # PRM_planner = PRM(map_array)
